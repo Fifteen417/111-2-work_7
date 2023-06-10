3d4a589a95a5b6978ca5a7727970969eaeaba222
Fifteen417 <vannessgoh888@gmail.com>
2023-06-10 14:50:57 +0800

    6/10 14.50
    finish internal alert

--- work_7/work_7/obj/Debug/work_7_Content.g.i.cs
+++ work_7/work_7/obj/Debug/work_7_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("clock.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("alert.wav")]
 
 
