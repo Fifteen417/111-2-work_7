53225e7e5fb6f656bbf31c0d60e9d538a4fb404d
Fifteen417 <vannessgoh888@gmail.com>
2023-06-10 14:48:41 +0800

    6/10 14.48
    finish outward

--- work_7/work_7/obj/Debug/MainWindow.g.i.cs
+++ work_7/work_7/obj/Debug/MainWindow.g.i.cs
@@ -40,6 +40,70 @@ namespace work_7 {
     /// </summary>
     public partial class MainWindow : System.Windows.Window, System.Windows.Markup.IComponentConnector {
         
+        
+        #line 10 "..\..\MainWindow.xaml"
+        [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1823:AvoidUnusedPrivateFields")]
+        internal System.Windows.Controls.TextBox time_txt;
+        
+        #line default
+        #line hidden
+        
+        
+        #line 11 "..\..\MainWindow.xaml"
+        [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1823:AvoidUnusedPrivateFields")]
+        internal System.Windows.Controls.TextBox date_txt;
+        
+        #line default
+        #line hidden
+        
+        
+        #line 12 "..\..\MainWindow.xaml"
+        [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1823:AvoidUnusedPrivateFields")]
+        internal System.Windows.Controls.TextBox week_txt;
+        
+        #line default
+        #line hidden
+        
+        
+        #line 13 "..\..\MainWindow.xaml"
+        [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1823:AvoidUnusedPrivateFields")]
+        internal System.Windows.Controls.Button setAlert_btn;
+        
+        #line default
+        #line hidden
+        
+        
+        #line 14 "..\..\MainWindow.xaml"
+        [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1823:AvoidUnusedPrivateFields")]
+        internal System.Windows.Controls.Button cancelAlert_btn;
+        
+        #line default
+        #line hidden
+        
+        
+        #line 15 "..\..\MainWindow.xaml"
+        [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1823:AvoidUnusedPrivateFields")]
+        internal System.Windows.Controls.ComboBox hour_cmb;
+        
+        #line default
+        #line hidden
+        
+        
+        #line 16 "..\..\MainWindow.xaml"
+        [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1823:AvoidUnusedPrivateFields")]
+        internal System.Windows.Controls.ComboBox min_cmb;
+        
+        #line default
+        #line hidden
+        
+        
+        #line 17 "..\..\MainWindow.xaml"
+        [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1823:AvoidUnusedPrivateFields")]
+        internal System.Windows.Controls.MediaElement sound_me;
+        
+        #line default
+        #line hidden
+        
         private bool _contentLoaded;
         
         /// <summary>
@@ -68,6 +132,33 @@ namespace work_7 {
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1800:DoNotCastUnnecessarily")]
         void System.Windows.Markup.IComponentConnector.Connect(int connectionId, object target) {
+            switch (connectionId)
+            {
+            case 1:
+            this.time_txt = ((System.Windows.Controls.TextBox)(target));
+            return;
+            case 2:
+            this.date_txt = ((System.Windows.Controls.TextBox)(target));
+            return;
+            case 3:
+            this.week_txt = ((System.Windows.Controls.TextBox)(target));
+            return;
+            case 4:
+            this.setAlert_btn = ((System.Windows.Controls.Button)(target));
+            return;
+            case 5:
+            this.cancelAlert_btn = ((System.Windows.Controls.Button)(target));
+            return;
+            case 6:
+            this.hour_cmb = ((System.Windows.Controls.ComboBox)(target));
+            return;
+            case 7:
+            this.min_cmb = ((System.Windows.Controls.ComboBox)(target));
+            return;
+            case 8:
+            this.sound_me = ((System.Windows.Controls.MediaElement)(target));
+            return;
+            }
             this._contentLoaded = true;
         }
     }
